db60ed498499f9acecd000c2442bcca40bc64371
Steven Seiden <53193445+StevenSeiden@users.noreply.github.com>
2023-06-07 11:26:07 -0500

    Added run script

---
+++ simulation/run.sh
@@ -0,0 +1,20 @@
+#Reset to origional state
+
+
+if [ "$3" != "-s" ]; then
+    echo "Performing cleanup."
+    git reset -–hard
+    git clean -fxd
+
+else
+    echo "Skipping cleanup."
+fi
+
+
+CC='gcc-5' CXX='g++-5' ./waf configure
+
+python run.py --cc poseidon --trace $1 --bw 100 --topo $2 --poseidon_m 0.25 --poseidon_min_rate 1.0 > ./results/data/$1-$2.txt
+
+cd results
+
+python draw.py
