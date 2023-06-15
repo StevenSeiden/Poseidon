eb9814432ee926a4f6ca080e293a6a0e50810c85
Steven Seiden <53193445+StevenSeiden@users.noreply.github.com>
2023-06-15 12:33:04 -0500

    Updated run and clean scripts to remove all old files properly

--- simulation/clean.sh
+++ simulation/clean.sh
@@ -1,10 +1,11 @@
 echo "Performing cleanup."
     rm results/data/$1-$2.txt
-    rm results/mpd/$1-$2.txt
-    rm results/rate/$1-$2.txt
-    rm results/signal/$1-$2.txt
-    rm results/smoothed_signal/$1-$2.txt
-    rm mix/config_$2_$1_poseidon.txt
-    rm mix/fct_$2_$1_poseidon.txt
-    rm mix/pfc_$2_$1_poseidon.txt
-    rm mix/qlen_$2_$1_poseidon.txt
+    rm results/mpd/$1-$2.png
+    rm results/rate/$1-$2.png
+    rm results/signal/$1-$2\_signals.png
+    rm results/smoothed_signal/$1-$2\_smooth.png
+    rm mix/config_$2\_$1\_poseidon.txt
+    rm mix/fct_$2\_$1\_poseidon.txt
+    rm mix/pfc_$2\_$1\_poseidon.txt
+    rm mix/qlen_$2\_$1\_poseidon.txt
+    rm mix/mix_$2\_$1\_poseidon.tr
--- simulation/run.sh
+++ simulation/run.sh
@@ -4,7 +4,7 @@
 
 CC='gcc-5' CXX='g++-5' ./waf configure
 
-python run.py --cc poseidon --trace $1 --bw 100 --topo $2 --poseidon_m 0.25 --poseidon_min_rate 1.0 > ./results/data/$1-$2.txt &
+python run.py --cc poseidon --trace $1 --bw 100 --topo $2 --poseidon_m 0.25 --poseidon_min_rate 1.0 > ./results/data/$1-$2.txt
 
 cd results
 
@@ -12,20 +12,21 @@ python draw.py
 
 cd ..
 
-echo "Perform cleanup (revert to last commit, deleting changes)? (y/n)"
+echo "Perform cleanup (deleting changes and results)? (y/n)"
 read answer
 
 if [ "$answer" == "y" ]; then
     echo "Performing cleanup."
     rm results/data/$1-$2.txt
-    rm results/mpd/$1-$2.txt
-    rm results/rate/$1-$2.txt
-    rm results/signal/$1-$2.txt
-    rm results/smoothed_signal/$1-$2.txt
-    rm mix/config_$2_$1_poseidon.txt
-    rm mix/fct_$2_$1_poseidon.txt
-    rm mix/pfc_$2_$1_poseidon.txt
-    rm mix/qlen_$2_$1_poseidon.txt
+    rm results/mpd/$1-$2.png
+    rm results/rate/$1-$2.png
+    rm results/signal/$1-$2\_signals.png
+    rm results/smoothed_signal/$1-$2\_smooth.png
+    rm mix/config_$2\_$1\_poseidon.txt
+    rm mix/fct_$2\_$1\_poseidon.txt
+    rm mix/pfc_$2\_$1\_poseidon.txt
+    rm mix/qlen_$2\_$1\_poseidon.txt
+    rm mix/mix_$2\_$1\_poseidon.tr
 else
     echo "Skipping the cleanup."
 fi
